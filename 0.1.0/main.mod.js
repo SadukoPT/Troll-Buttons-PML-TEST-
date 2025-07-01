c0ee106387a4ce2dc3c074e33d6f8ca7d8d7c023
SadukoPT <totkbotwloz@gmail.com>
2025-07-01 09:50:15 +0530

    Update main.mod.js

--- 0.1.0/main.mod.js
+++ 0.1.0/main.mod.js
@@ -2,12 +2,13 @@ import { PolyMod, MixinType, PolyModLoader } from "https://pml.orangy.cfd/PolyTr
 
 class TrollButtonsMod extends PolyMod {
     init = function(pml) {
-  pml.registerSettingCategory("Add Speed Mod");
-
-  //setting name, id, eventtype, default value1, default value2 (optional), function
-
-  pml.registerSetting("Add 100 Speed", "modAdd100", "keydown", "KeyM", "KeyN", (e) => {
+      pml.registerSettingCategory("Add Speed Mod");
     
+      //setting name, id, eventtype, default value1, default value2 (optional), function
+    
+      pml.registerSetting("Add 100 Speed", "modAdd100", "keydown", "KeyM", "KeyN", (e) => {
+        
       });
-
+    }
+}
 export const polyMod = new TrollButtonsMod();
